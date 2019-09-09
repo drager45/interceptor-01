3c6dd00fe11e2de39e0296cf2aefd3f44b1840a9
Wolf <drager45@hotmail.com>
2019-09-08 20:27:50 -0500

    Se agrega el manejo de errores al interceptor

--- src/app/interceptors/interceptor.service.ts
+++ src/app/interceptors/interceptor.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest , HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpInterceptor, HttpRequest , HttpHandler, HttpEvent, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +14,23 @@ export class InterceptorService implements HttpInterceptor {
   // next: es lo que se debe ejecutar en cuanto se ejecute el interceptor
   // Observable<HttpEvent<any>>: es el tipado del retorno del interceptor
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    
-    return next.handle( req );
+
+    const headers = new HttpHeaders({
+      'token-usuario': 'ABC45450405'
+    });
+    const reqClone = req.clone({
+      headers
+    });
+    return next.handle( reqClone ).pipe(
+      catchError( this.controlErrors)
+    );
   }
+
+  controlErrors(error: HttpErrorResponse) {
+    console.log('Ocurrio un error ');
+    console.log('Registrado en el log file');
+    console.warn(error);
+    return throwError('Error en la url de la peticion')
+  }
+
 }
--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { map, catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,27 +10,13 @@ export class UsuariosService {
   constructor(private http: HttpClient) { }
 
   obtenerUsuarios() {
-    let  params = new HttpParams().append('page','1');
+    let  params = new HttpParams().append('page','2');
     params= params.append('nombre','Fernando Herrera');
 
-    //const headers = new HttpHeaders({
-    //  'token-usuario': 'ABC45450405'
-    //});
-
-    return this.http.get(`https://reqres.in/api/user`, {
+    return this.http.get(`https://reqres1212.in/api/user`, {
       params
     }).pipe(
-
       map(resp => resp['data']),
-      catchError(this.controlErrors)
-
     );
-  }
-
-  controlErrors(error: HttpErrorResponse) {
-    console.log('Ocurrio un error ');
-    console.log('Registrado en el log file');
-    console.warn(error);
-    return throwError('Error en la url de la peticion')
   }
 }
