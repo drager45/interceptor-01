1f5d2415ed8f0cc0898c2196fc6a63dea222a14b
Wolf <drager45@hotmail.com>
2019-09-08 20:00:39 -0500

    Se configura el interceptor en el app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { InterceptorService } from './interceptors/interceptor.service';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +13,11 @@ import { AppComponent } from './app.component';
     BrowserModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: InterceptorService,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/interceptors/interceptor.service.ts
+++ src/app/interceptors/interceptor.service.ts
@@ -12,8 +12,7 @@ export class InterceptorService implements HttpInterceptor {
   // next: es lo que se debe ejecutar en cuanto se ejecute el interceptor
   // Observable<HttpEvent<any>>: es el tipado del retorno del interceptor
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log('Se evalua por el Interceptor');
+    
     return next.handle( req );
   }
-
 }
